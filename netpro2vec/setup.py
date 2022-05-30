11b59f4f400908978462b632d66a040477761d75
Maurizio Giordano <maurizio@iMac-di-Maurizio.local>
2022-05-30 10:54:18 +0200

    new

--- netpro2vec/setup.py
+++ netpro2vec/setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup
 
  setup(
-   name='etpro2vec',
+   name='Netpro2Vec_nx',
    version='0.1.0',
-   author='Ichcha Manipur and Maurizio Giordano',
-   author_email='ichcha.manipur@icar.cnr.it',
+   author='Maurizio Giordano',
+   author_email='maurizio.giordano@icar.cnr.it',
    packages=['package_name', 'package_name.test'],
    #scripts=['bin/script1','bin/script2'],
    #url='http://pypi.python.org/pypi/PackageName/',
    license='LICENSE.txt',
    description='The netpro2vec graph-embedding method',
-   long_description=open('README.txt').read(),
+   long_description=open('README.md').read(),
    install_requires=[
-       "python-igraph",
        "tqdm",
        "pandas",
        "scipy"
