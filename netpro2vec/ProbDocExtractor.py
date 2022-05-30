6c618edadccc70c944b55bd8f2834f9e285dbb69
Maurizio Giordano <maurizio@iMac-di-Maurizio.local>
2022-05-30 12:14:33 +0200

    new

--- netpro2vec/ProbDocExtractor.py
+++ netpro2vec/ProbDocExtractor.py
@@ -18,7 +18,7 @@ class ProbDocExtractor:
 	specify extractor for ordered/unordered/rounded/tagged/untagged
 	"""
 	def __init__(self, probability_distrib_matrix, doc_tag, word_tag=None,
-				 extractor=1, tag=True, encodew=True, vertex_label_path=None):
+				 extractor=1, tag=True, encodew=True, vertex_labels=None):
 		self.probability_distrib_matrix = probability_distrib_matrix
 		self.word_tag = word_tag
 		self.doc_tag = doc_tag
@@ -28,21 +28,13 @@ class ProbDocExtractor:
 		self.graph_document = []
 		self.extractor = extractor
 		self.tag = tag
-		self.vertex_label_path = vertex_label_path
+		self.vertex_labels = vertex_labels
 		if self.extractor == 1:
 			self.ordered_probability_extractor()
 		elif self.extractor == 2:
-			self.prob_to_string_feature_extractor()
-		elif self.extractor == 3:
-			self.unordered_probability_extractor()
-		elif self.extractor == 4:
-			self.unordered_probability_extractor_round()
-		elif self.extractor == 5:
-			self.ordered_probability_extractor_round()
-		elif self.extractor == 6:
 			self.ordered_probability_extractor_multi()
 		else:
-			raise Exception("extractor level is in the range [1, 6]")
+			raise Exception("extractor level is in the range [1, 2]")
 
 	def ordered_probability_extractor(self):
 		"""
@@ -56,62 +48,6 @@ class ProbDocExtractor:
 			self.probability_distrib_matrix, axis=1))
 		features_graph = probability_distrib_matrix.tolist()
 		cut = (self.probability_distrib_matrix > 0).sum(axis=1)
-		self.features_graph = [features_graph[i][0:cut[i]] for i in range(0,
-																	  len(cut))]
-		self.get_graph_document()
-
-	def prob_to_string_feature_extractor(self):
-		"""
-		Node-wise probability values for each rooted node converted to words
-		Does not really work, does not create high frequency words,
-		so no vocabulary created.
-		"""
-		probability_distrib_matrix = self.probability_distrib_matrix * 1000
-		probability_distrib_matrix = probability_distrib_matrix.astype(int)
-		self.features_graph = probability_distrib_matrix.tolist()
-		self.get_graph_document()
-
-	def unordered_probability_extractor(self):
-		"""
-		Unorodered sequence of nodes (non-neg probabilities) or bin index (of
-		distances or node labels) for each rooted node converted to words. For
-		LFR,
-		unweighted synthetic data.
-		"""
-		features_graph = self.probability_distrib_matrix.tolist()
-		cut = (self.probability_distrib_matrix > 0).sum(axis=1)
-		self.features_graph = [np.array(np.nonzero(features_graph[
-													   i])).flatten().tolist()
-							   for i in
-							   range(0, len(cut))]
-		self.get_graph_document()
-
-	def unordered_probability_extractor_round(self):
-		"""
-		Ordered sequence (decreasing probability) of nodes or bin index (of
-		distances) for each rooted node converted to words.  round to 2,
-		to be tested properly.
-		"""
-		probability_distrib_matrix = np.round(
-			self.probability_distrib_matrix, 2)
-		features_graph = probability_distrib_matrix.tolist()
-		cut = (probability_distrib_matrix > 0).sum(axis=1)
-		self.features_graph = [features_graph[i][0:cut[i]] for i in range(0,
-																	  len(cut))]
-		self.get_graph_document()
-
-	def ordered_probability_extractor_round(self):
-		"""
-		Ordered sequence (decreasing probability) of nodes or bin index (of
-		distances) for each rooted node converted to words. round to 2,
-		to be tested
-		"""
-		probability_distrib_matrix = np.round(
-			self.probability_distrib_matrix, 2)
-		probability_distrib_matrix = np.fliplr(np.argsort(
-			probability_distrib_matrix, axis=1))
-		features_graph = probability_distrib_matrix.tolist()
-		cut = (probability_distrib_matrix > 0).sum(axis=1)
 		self.features_graph = [features_graph[i][0:cut[i]] for i in range(0,
 																	  len(cut))]
 		self.get_graph_document()
@@ -153,27 +89,31 @@ class ProbDocExtractor:
 
 	def get_graph_document(self):
 		if self.word_tag is not None:
-			if self.vertex_label_path is not None and 'tm' in self.word_tag:
-				vertex_labels = pd.read_csv(self.vertex_label_path).iloc[:,
-								0].tolist()
+			if self.vertex_labels is not None and 'tm' in self.word_tag:
+				vertex_labels = self.vertex_labels
 				self.features_graph = [[vertex_labels[i] for i in
 									self.features_graph[x]]
 								  for x in range(0, len(self.features_graph))]
 				[self.features_graph[i].insert(0, self.word_tag + '_' +
 											   str(vertex_labels[i])) for i in
 				 range(0, len(vertex_labels))]
-			elif self.vertex_label_path is not None and self.word_tag == 'ndd':
-				vertex_labels = pd.read_csv(self.vertex_label_path).iloc[:,
-								0].tolist()
+			elif self.vertex_labels is not None and self.word_tag == 'ndd':
+				vertex_labels = self.vertex_labels
 				[self.features_graph[i].insert(0, self.word_tag + '_' + str(
 					vertex_labels[i]))
 				 for i in range(0, len(vertex_labels))]
-			elif self.vertex_label_path is None:
-				[self.features_graph[i].insert(0, self.word_tag + '_' + str(i)) for i in
-				 range(0, len(self.features_graph))]
+			elif self.vertex_labels is None:
+				[self.features_graph[i].insert(0, self.word_tag + '_' + str(
+					i)) for i in range(0, len(self.features_graph))]
 		new_ind_list_node_seq = [("_".join(str(item) for item in new_ind)) for
 								 new_ind in self.features_graph]
 
+		# # uncomment to keep only unique words
+		# unique_words = []
+		# [unique_words.append(item) for item in new_ind_list_node_seq if item not in
+		#  unique_words]
+		# new_ind_list_node_seq = unique_words
+		# print(new_ind_list_node_seq)
 
 		if self.encode:
 			hash_object_list = [hashlib.md5(features.encode()) for features in
@@ -185,7 +125,7 @@ class ProbDocExtractor:
 
 		if self.tag:
 			self.graph_document = self.get_tagged_doc(self.graph_document)
-		if self.extractor <= 5:
+		if self.extractor == 1:
 			self.probability_distrib_matrix = []
 			self.features_graph = []
 
