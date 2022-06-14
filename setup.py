c3852ec85cce87ba28e84bee6bc9f6d95e7fa190
Maurizio Giordano <maurizio.giordano@cnr.it>
2022-06-14 10:02:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
    version='0.1.0',
    author='Maurizio Giordano',
    author_email='maurizio.giordano@icar.cnr.it',
-   packages=['netpro2vec'],
+   packages=['netpro2vec_nx'],
    #scripts=['bin/script1','bin/script2'],
    #url='http://pypi.python.org/pypi/PackageName/',
    license='LICENSE.txt',
@@ -13,6 +13,7 @@ setup(
    long_description=open('README.md').read(),
    install_requires=[
        "networkx",
+       "gensim",
        "tqdm",
        "pandas",
        "scipy"
