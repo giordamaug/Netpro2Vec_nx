e2178c48f9e557ae14b3a63166f41c5b823cd4dd
Maurizio Giordano <maurizio.giordano@cnr.it>
2022-06-14 10:07:36 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import sys,os
 import time
 import sys
 sys.path.append('.')
-from netpro2vec_nx.netpro2vec import Netpro2Vec
+from netpro2vec_nx.Netpro2Vec import Netpro2Vec
 import tqdm
 import csv
 
